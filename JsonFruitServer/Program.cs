b3272507130a57bf897081ce35c499f81b396c16
zealand <zealand@ZEA-3603>
2021-09-07 15:21:43 +0200

    Add project files.

---
+++ JsonFruitServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+using JsonFruit;
+
+namespace JsonFruitServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fruit server ready");
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 10002);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Console.WriteLine("New client");
+
+                Task.Run(() =>
+                {
+                    HandleClient(socket);
+                });
+            }
+        }
+
+        private static void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            string message = reader.ReadLine();
+
+            Fruit fromJson = JsonSerializer.Deserialize<Fruit>(message);
+            Console.WriteLine("Frugt type: " + fromJson.typeOfFruit);
+            writer.WriteLine("Frugt modtaget");
+            writer.Flush();
+            socket.Close();
+        }
+    }
+}
